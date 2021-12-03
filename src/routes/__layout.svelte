7f20933ce9425c0e0b6127e73ea4416cae990e34
Claudia Valdivieso <claumavaldivieso@gmail.com>
2021-12-03 17:53:47 -0500

    improve the layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,19 @@
 <script>
-	import Nav from '../components/nav.svelte';
+  import Nav from '../components/nav.svelte';
 </script>
 
 <div class="p-8 max-w-6xl mx-auto">
-	<Nav />
-	<slot />
+  <Nav />
+  <slot />
 </div>
 
 <style>
-	@tailwind base;
-	@tailwind components;
-	@tailwind utilities;
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+  :global(body) {
+    background-image: url(/header-bg.png), url(/bg-squares.png);
+    background-repeat: no-repeat, repeat;
+  }
 </style>
